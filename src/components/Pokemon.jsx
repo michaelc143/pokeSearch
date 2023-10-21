c81347aaf52f39221388ff83deae59d908931a65
Michael Corbishley <mcorbishley@wisc.edu>
2023-10-20 19:01:43 -0500

    fixing capitalize

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,25 +1,32 @@
 import React from "react";
-import { Card, Container } from "react-bootstrap";
+import { Card, Container, Stack, Badge } from "react-bootstrap";
 
 function Pokemon(props) {
 
-    let name = props.name;
+    const capitalize = (toCaps) => {
+        return toCaps.charAt(0).toUpperCase() + toCaps.slice(1);
+    };
 
     return(
         <Container style={{ width: '18rem' }}>
             <Card style={{margin: "0.5rem", padding: "0.5rem"}}>
                 <Card.Img variant="top" src={props.photoUrl} />
                 <Card.Title>
-                    {name}
+                    {capitalize(props.name)}
                 </Card.Title>
                 <Card.Body>
                     <p>Id: {props.id}</p>
                     <p>Types: </p>
+                    <Stack>
                     {
                         props.types.map((type) => {
-                            return <ul key={type.type.name}>{type.type.name}</ul>
+                            return <>
+                                <Badge key={type.type.name}>{capitalize(type.type.name)}</Badge>
+                                <br/>
+                            </>
                         })
                     }
+                    </Stack>
                 </Card.Body>
             </Card>
         </Container>
